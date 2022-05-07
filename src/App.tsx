08a508e159b5fe1910f8137c47c62698f7ca77f2
Ivan <glhf@vk.com>
2022-05-08 00:16:44 +0700

    feat: app bg, cell and field cosmetic changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import {Field} from "./features/field/Field";
-
+import styles from './App.module.css'
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
       <Field />
     </div>
   );
