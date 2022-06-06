9a0f4272feacd53d6b19fb2ddc81e90dcb4d69d0
Ivan <glhf@vk.com>
2022-06-06 20:30:34 +0700

    feat: board rotation enabled by default

--- src/features/options/optionsSlice.ts
+++ src/features/options/optionsSlice.ts
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../../app/store";
 
 const initialState = {
-  ENABLE_ROTATION: false
+  ENABLE_ROTATION: true
 }
 
 const optionsSlice = createSlice({
